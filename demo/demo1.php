608de8b5025a40684c360adf6d766d64bce32213
程金盛 <jelly@chengjinshengdeMacBook-Pro.local>
2016-08-15 10:59:56 +0800

    mime type

--- demo/demo1.php
+++ demo/demo1.php
@@ -11,8 +11,8 @@ $reg = "/\.(js|css|jpg|jpeg|gif|png|ico|swf|bmp|tiff|svg|doc|docx|txt|rtf|pdf|xl
 if(preg_match($reg, strrchr($uriPath, '.'))) {//对静态资源
     $mimeTypesObj = new \CjsMime\MimeTypes();
 	$mimeType = $mimeTypesObj->getMimeType(\CjsMime\MimeTypes::getFileExt($filename));
-	echo $mimeType;
+	echo $mimeType . PHP_EOL;
 	if($mimeType) {
-		header('Content-type: ' . $mimeType);
+		//header('Content-type: ' . $mimeType);
 	}
 }
